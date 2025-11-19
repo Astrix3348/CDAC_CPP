3177f4939fb45000fad42b61a9d0f540cc65e764
Aakash Gangwar <90668043+Astrix3348@users.noreply.github.com>
2025-11-19 12:01:08 +0530

    Improve digit frequency counting in Q6.cpp
    Refactor digit counting and frequency calculation logic.

--- C++/Q6.cpp
+++ C++/Q6.cpp
@@ -7,30 +7,25 @@ int main(){
 	int n;
 	cin >> n;
 	int num = n;
+
+	int arr[10] = {0,0,0,0,0,0,0,0,0,0};
+
 	int count = 0;
-	while(n > 0){
+
+	while(n>0){
 		count++;
 		n = n/10;
 	}
 
-	int arr[count];
-	for(int i = 0; i < count; i++){
-		arr[i] = num%10;
+	while(num>0){
+		int digit = num%10;
+		arr[digit]++;
 		num = num/10;
 	}
-	
 
-	int freq = 1;
-	for(int i = 1; i < count; i++){
-		if(arr[i-1]!=arr[i]){
-			freq = 1;
-		}
-		else{
-			freq++;
-		}
+	for(int i = 0; i < 10; i++){
+		cout << i << "-->" << arr[i] << endl;
 	}
-
-	cout << freq << endl;
 	
 	return 0;
 }
